8fc18b6fbdd15e47813ee3c2ccfb0abc143c3559
Roman <romanwn@mail.com>
2023-03-08 15:06:03 +0100

    moving

--- beer_tap/tests/test_responses.py
+++ beer_tap/tests/test_responses.py
@@ -1,25 +1,4 @@
-import pytest
-# import requests_mock
-# from .._module_interface import *
-from app import app
-from beer_tap._module_interface import *
 
-
-
-# def test_a(mocker):
-#     flow_volume = 0.0
-#     mocker.patch
-#     with requests_mock.Mocker() as rm:
-#         rm.put(mock_address, json=mock_response, status_code=200)
-#         response = admin_elasticsearch.create_index("test_index")
-#         assert response == mock_response
-
-
-# def test_new_dispenser(requests_mock):
-#     flow_volume = 0.1
-#     requests_mock.get(f'{__BASE_URL}/employee/{test_id}', json= {'name': 'awesome-mock'})
-#     resp = get_employee('random-id')
-#     assert resp == {'name': 'awesome-mock'}
 
 def test_new_disp(mocker):
     mocker.patch(
